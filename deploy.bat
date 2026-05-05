73b39aeb4fdccca64ad7a300a8deb2d06a545512
Your Name <you@example.com>
2026-05-05 23:09:07 +0800

    Auto update 週二 05/05/2026 23:09:07.05

--- deploy.bat
+++ deploy.bat
@@ -1,13 +1,13 @@
 @echo off
-echo 🚀 ready to update...
+echo ready to update...
 git add .
 
 set commit_msg="Auto update %date% %time%"
 git commit -m %commit_msg%
 
-echo 📤 pushing GitHub...
+echo pushing GitHub...
 git push origin main
 
-echo ✅ Finished！GitHub Actions deploying。
+echo Finished！GitHub Actions deploying。
 echo 30sec to refresh。
 pause
